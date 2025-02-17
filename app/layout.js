99b12116f85d619210baecc5bc351fdb51754c2b
bhaveshkl <kodlibhavish@gamil.com>
2025-02-17 15:19:26 +0530

    Specify MIME type for favicon in layout metadata

--- app/layout.js
+++ app/layout.js
@@ -72,7 +72,7 @@ export default async function RootLayout({ children }) {
           content="N4FxqHbSvySNNCTrB03INB_j9KrcdSmEUPhhlh-UQsA" 
         />
         <meta name="application-name" content="DontQue" />
-        <link rel="icon" href="/default.jpg" />
+        <link rel="icon" type="image/jpeg" href="/default.jpg" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
